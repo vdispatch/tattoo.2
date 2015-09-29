eefd6b611cda0e29f287776c70f39e6268b3194a
Prelang Builder <builder@prelang.com>
2015-09-29 09:14:20 +0000

    Installed relation 'has_and_belongs_to_many' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
   #->Prelang (user_login/devise)
   has_many :projects
+  has_and_belongs_to_many :user_badges
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
 
