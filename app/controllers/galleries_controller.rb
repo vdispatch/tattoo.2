7613b4177c17c0336c13e67419133f63f543913c
Prelang Builder <builder@prelang.com>
2015-09-29 09:15:14 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,5 @@
 class GalleriesController < ApplicationController
-  before_action :set_gallery, only: [:show, :edit, :update, :destroy]
+  before_action :set_gallery, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /galleries
   # GET /galleries.json
